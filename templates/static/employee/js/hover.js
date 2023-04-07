650ad93bd60c8bb76fdd251a5ff9a7235b656220
MarcelMotta-J <marceldsmotta@gmail.com>
2023-04-07 10:22:13 -0300

    first commit

---
+++ templates/static/employee/js/hover.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    // show buttons on tr mouseover
+    $(".hover tr").live("mouseenter", function() {
+      $(this).find("td:last-child").html('<a href="javascript:void(0);" onClick="editrow(' + $(this).attr("id") + ')">Edit</a>&nbsp;&nbsp;<a href="javascript:void(0);" onClick="deleterow(' + $(this).attr("id") + ')">Delete</a>');
+    }); //
+  
+    // remove button on tr mouseleave
+    $(".hover tr").live("mouseleave", function() {
+      $(this).find("td:last-child").html("&nbsp;");
+    });
+  
+    // TD click event
+    $(".hover tr").live("click", function(event) {
+      if (event.target.nodeName == "TD") {
+        alert("You can track TD click event too....Isnt it amazing !!!");
+      }
+    });
+  });
+  editrow = function(itemId) {
+    alert("You clicked 'Edit' link with row id :" + itemId);
+  }
+  deleterow = function(itemId) {
+    alert("You clicked 'Delete' link with row id :" + itemId);
+  }
